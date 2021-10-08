c6ed0c6aa29d7a104123bc54e6ce7ff8e7fb9c12
Mike Kutuzov <mikealtkutuzov@gmail.com>
2021-10-08 18:26:21 +0300

    fix2

--- twntyx2p/maingame/models.py
+++ twntyx2p/maingame/models.py
@@ -4,7 +4,7 @@ from twntyx2p.accounts.models import User
 
 
 class UserGame(models.Model):
-    user_id = models.ForeignKey(User.id,primary_key=True, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,primary_key=True, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     total_time_run = models.FloatField(default=0.0)
 
