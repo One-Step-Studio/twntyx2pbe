8f30890435d1ba29c9157e324763cee2336cac7e
Mike Kutuzov <mikealtkutuzov@gmail.com>
2021-10-14 13:57:44 +0300

    they used to call me prophet. psychotic prophet

--- twntyx2p/accounts/models.py
+++ twntyx2p/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser
+from user_manager import  UserManager
 
 
 class User(AbstractBaseUser):
